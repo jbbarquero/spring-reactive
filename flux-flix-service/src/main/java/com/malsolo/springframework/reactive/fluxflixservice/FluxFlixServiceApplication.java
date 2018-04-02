d5314986b70a4b035ea3e2dda0139e408689bffd
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-04-02 11:24:11 +0200

    spring-reactive: service from start.spring.io

---
+++ flux-flix-service/src/main/java/com/malsolo/springframework/reactive/fluxflixservice/FluxFlixServiceApplication.java
@@ -0,0 +1,12 @@
+package com.malsolo.springframework.reactive.fluxflixservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxFlixServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxFlixServiceApplication.class, args);
+	}
+}
