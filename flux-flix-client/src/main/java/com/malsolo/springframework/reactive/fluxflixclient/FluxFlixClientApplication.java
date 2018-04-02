1c688a68ef7d62761e2657f5f38b52ae59cca1fc
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-04-02 12:26:24 +0200

    spring-reactive: client from start.spring.io

---
+++ flux-flix-client/src/main/java/com/malsolo/springframework/reactive/fluxflixclient/FluxFlixClientApplication.java
@@ -0,0 +1,12 @@
+package com.malsolo.springframework.reactive.fluxflixclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxFlixClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxFlixClientApplication.class, args);
+	}
+}
