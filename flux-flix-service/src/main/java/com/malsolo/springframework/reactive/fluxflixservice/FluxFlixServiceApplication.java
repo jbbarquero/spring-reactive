4bcb14a2636e55b34f0ea1df182042346fd460c9
Javier Beneito Barquero <jbbarquero@gmail.com>
2018-04-02 12:13:44 +0200

    spring-reactive: service with stream generator

--- flux-flix-service/src/main/java/com/malsolo/springframework/reactive/fluxflixservice/FluxFlixServiceApplication.java
+++ flux-flix-service/src/main/java/com/malsolo/springframework/reactive/fluxflixservice/FluxFlixServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,7 +17,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
+import java.time.Duration;
+import java.time.LocalDate;
 import java.util.Random;
 import java.util.UUID;
 import java.util.stream.Stream;
@@ -53,6 +57,11 @@ class MovieRestController {
 		this.fluxFlixService = fluxFlixService;
 	}
 
+	@GetMapping(value = "/{id}/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<MovieEvent> events(@PathVariable String id) {
+		return this.fluxFlixService.byId(id).flatMapMany(this.fluxFlixService::streamStreams);
+	}
+
 	@GetMapping
 	public Flux<Movie> all() {
 		return this.fluxFlixService.all();
@@ -72,6 +81,14 @@ class FluxFlixService {
 		this.movieRepository = movieRepository;
 	}
 
+	public Flux<MovieEvent> streamStreams(Movie movie) {
+	    Flux<Long> interval = Flux.interval(Duration.ofSeconds(1));
+
+	    Flux<MovieEvent> events = Flux.fromStream(Stream.generate(() -> new MovieEvent(movie, LocalDate.now(), randomUser())));
+
+	    return Flux.zip(interval, events).map(Tuple2::getT2);
+    }
+
 	public Flux<Movie> all() {
 		return this.movieRepository.findAll();
 	}
@@ -84,6 +101,13 @@ class FluxFlixService {
 		String[] users = "user1,user2,user3,user5".split(",");
 		return users[new Random().nextInt(users.length)];
 	}
+}
+
+@Data
+class MovieEvent {
+    private final Movie movie;
+    private final LocalDate when;
+    private final String user;
 }
 
 interface MovieRepository extends ReactiveMongoRepository<Movie, String> {
